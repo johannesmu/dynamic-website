202fda6682209795a87b388b088c59275973c6db
Johannes <jmuljana@gmail.com>
2024-11-27 05:04:37 +0000

    adding appwrite

--- src/App.jsx
+++ src/App.jsx
@@ -10,20 +10,29 @@ import {Home} from './pages/Home'
 import {About} from './pages/About'
 import {Signup} from './pages/Signup'
 import {Routes,Route} from 'react-router-dom'
+import { AuthContext } from './context/AuthContext';
 
+import { Client, Account } from 'appwrite';
+import { APIKEY } from './config/Key';
 
 function App() {
 
-  
+  const client = new Client()
+  client.setProject('64645db2622965d36e51')
+  client.setEndpoint('https://cloud.appwrite.io/v1')
+
+  const account = new Account(client)
 
   return (
     <>
-      <Header text="Wine" />
+    <AuthContext.Provider value={ account }>
+      <Header text="Wine Company" />
       <Routes>
         <Route path='/' element={ <Home/> } />
         <Route path="/about" element={ <About/> } />
         <Route path='/register' element={ <Signup/> } />
       </Routes>
+      </AuthContext.Provider>
       
     </>
   )
