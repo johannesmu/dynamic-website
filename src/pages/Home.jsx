84e2dc948cbd141275d4edec5f7d2808754b15da
Johannes <jmuljana@gmail.com>
2024-11-27 03:57:54 +0000

    adding routes

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+export function Home( props ) {
+    document.title = "The best drinks in town"
+    return(
+        <Container>
+            <Row>
+                <Col>
+                    <h2>Home</h2>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
