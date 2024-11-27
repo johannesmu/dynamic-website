69f51cbad6e6b47d2a30a8fe3b729f90bfadecd1
Johannes <jmuljana@gmail.com>
2024-11-27 08:52:44 +0000

    adding appwrite db

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,15 @@ import {Home} from './pages/Home'
 import {About} from './pages/About'
 import {Signup} from './pages/Signup'
 import {Routes,Route} from 'react-router-dom'
+import { Client, Databases } from 'appwrite'
 
 
 function App() {
+  const client = new Client();
+  client.setProject('6746c3630001c01c783e');
+  client.setEndpoint("https://cloud.appwrite.io/v1")
 
+  const database = new Databases( client )
   
 
   return (
